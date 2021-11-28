03bf2a2cd316d8548372e7585f7929de688146dd
Joan Sebastián Giraldo Morales <sebastiangiraldo3@gmail.com>
2021-11-27 19:42:15 -0500

    actualizacion de vistas

--- lib/src/views/InfoMatchPage.dart
+++ lib/src/views/InfoMatchPage.dart
@@ -30,7 +30,7 @@ class _InfoMatchPageState extends State<InfoMatchPage> {
                   padding: const EdgeInsets.all(20.0),
                   child: Container(
                     width: size.width * 0.40,
-                    height: size.height * 0.60,
+                    height: size.height * 0.70,
                     decoration: const BoxDecoration(
                         color: Colors.transparent,
                         borderRadius: BorderRadius.only(
@@ -53,14 +53,14 @@ class _InfoMatchPageState extends State<InfoMatchPage> {
                 ),
                 Container(
                   width: size.width * 0.50,
-                  height: size.height * 0.60,
+                  height: size.height * 0.70,
                   decoration: BoxDecoration(
                       color: const Color.fromRGBO(35, 40, 50, 1.0),
                       borderRadius: BorderRadius.circular(15)),
                   child: Column(
                     children: [
                       const Padding(
-                        padding:  EdgeInsets.all(15.0),
+                        padding:  EdgeInsets.all(10.0),
                         child:  Text(
                           'Datos principales del inmueble',
                           style: TextStyle(
@@ -477,7 +477,7 @@ class _InfoMatchPageState extends State<InfoMatchPage> {
                         ),
                       ),
                       const Padding(
-                        padding:  EdgeInsets.all(15.0),
+                        padding:  EdgeInsets.all(10.0),
                         child:  Text(
                           'Características del inmueble:',
                           style: TextStyle(
@@ -693,6 +693,132 @@ class _InfoMatchPageState extends State<InfoMatchPage> {
                   ),
                 ),
               ],
+            ),
+            // Row(
+            //   children: [
+            //     Container(
+            //       width: double.infinity,
+            //       height: size.height*0.30,
+            //       decoration: BoxDecoration(
+            //         color: const Color.fromRGBO(35, 40, 50, 1.0),
+            //         borderRadius: BorderRadius.circular(15)
+            //       ),
+            //       child:Column(
+            //         crossAxisAlignment: CrossAxisAlignment.start,
+            //         children: [
+            //           const Text(
+            //             'Ubicacion de la Propiedad:',
+            //             style: TextStyle(
+            //               color: Colors.white,
+            //               fontWeight: FontWeight.bold,
+            //               fontSize: 18,
+            //             ),
+            //           ),
+            //           Container(
+            //             width: size.width*0.20,
+            //             height: size.height*0.25,
+            //             decoration: BoxDecoration(
+            //               color: Colors.transparent,
+            //               borderRadius: BorderRadius.circular(15)
+            //             ),
+            //             child: GestureDetector(
+            //               onDoubleTap: () => Navigator.pushNamed(context, 'MapePageView'),
+            //               child: ClipRRect(
+            //                 child: const GeoLocationMap(),
+            //                 borderRadius:BorderRadius.circular(15)
+            //               ),
+            //             ),
+            //           ),
+            //         ],
+            //       )
+            //     ),
+            //   ],
+            // )
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      decoration:const BoxDecoration(
+                        color: Color.fromRGBO(57, 63, 70, 1),
+                      ),
+                      child: IconButton(
+                        onPressed: (){},
+                        icon: const Icon(
+                          CupertinoIcons.clear_thick,
+                          color: Color.fromRGBO(254, 69, 82, 1),
+                          size: 40,
+                        )
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0,right: 10.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration:const BoxDecoration(
+                          color: Color.fromRGBO(57, 63, 70, 1),
+                        ),
+                        child: IconButton(
+                          onPressed: (){},
+                          icon: const Icon(
+                            CupertinoIcons.chat_bubble_2_fill,
+                            color: Colors.white,
+                            size: 40,
+                          )
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left:10.0,right:20.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Container(
+                        width: 60,
+                        height: 60,
+                        decoration:const BoxDecoration(
+                          color: Color.fromRGBO(57, 63, 70, 1),
+                        ),
+                        child: IconButton(
+                          onPressed: ()=> Navigator.pushNamed(context, 'MapePageView'),
+                          icon: const Icon(
+                            CupertinoIcons.location_solid,
+                            color: Colors.yellow,
+                            size: 40,
+                          )
+                        ),
+                      ),
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      decoration:const BoxDecoration(
+                        color: Color.fromRGBO(57, 63, 70, 1),
+                      ),
+                      child: IconButton(
+                        onPressed: (){},
+                        icon: const Icon(
+                          CupertinoIcons.heart_fill,
+                          color: Color.fromRGBO(27, 175, 138, 1),
+                          size: 40,
+                        )
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
